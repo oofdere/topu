070faee04d51eabfb43e8ad7a31761262b269d0a
oofdere <oofdere@hey.com>
2026-02-19 16:18:31 -0800

    initial grammar

--- packages/language/src/topu-validator.ts
+++ packages/language/src/topu-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { TopuAstType, Person } from './generated/ast.js';
+import type { ValidationChecks } from 'langium';
+import type { TopuAstType } from './generated/ast.js';
 import type { TopuServices } from './topu-module.js';
 
 /**
@@ -9,7 +9,7 @@ export function registerValidationChecks(services: TopuServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.TopuValidator;
     const checks: ValidationChecks<TopuAstType> = {
-        Person: validator.checkPersonStartsWithCapital
+        // Person: validator.checkPersonStartsWithCapital
     };
     registry.register(checks, validator);
 }
@@ -19,13 +19,13 @@ export function registerValidationChecks(services: TopuServices) {
  */
 export class TopuValidator {
 
-    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-        if (person.name) {
-            const firstChar = person.name.substring(0, 1);
-            if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
-            }
-        }
-    }
+    // checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+    //     if (person.name) {
+    //         const firstChar = person.name.substring(0, 1);
+    //         if (firstChar.toUpperCase() !== firstChar) {
+    //             accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //         }
+    //     }
+    // }
 
 }
